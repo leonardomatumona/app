4ee1917da5cfe2941150dfe46964183e623de862
leonardomatumona <leonardomatumona@gmail.com>
2020-07-15 20:11:43 -0300

    Add project files.

---
+++ app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var evento = new
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
